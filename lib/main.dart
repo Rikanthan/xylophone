f98eaf49bc23802ed2184357d0f5153504d986d6
ricky <rikanthanricky@gmail.com>
2020-02-08 20:51:34 +0530

    final

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,30 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(xylo());
 
+
+void playsound(int c)
+{
+
+    final player = AudioCache();
+    player.play('note$c.wav');
+
+}
+Expanded buildkey({Color color,int soundnumber})
+{
+  return Expanded(
+      child:FlatButton(
+        color: color,
+      onPressed:() {
+
+        playsound(soundnumber);
+
+          },
+                   ),
+
+
+    );
+}
+
 class xylo extends StatelessWidget {
 
   @override
@@ -21,129 +45,13 @@ class xylo extends StatelessWidget {
               (
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
-
-                Container(
-
-              height: 75.0,
-
-              color:Colors.blueAccent,
-
-
-                  child:FlatButton(
-                    onPressed:() {
-                      final player = AudioCache();
-                      player.play('note1.wav');
-                    },
-                  ),
-                ),
-                SizedBox(
-                  height: 10.0,
-                ),
-                Container(
-
-                  height: 75.0,
-
-                  color:Colors.yellowAccent,
-
-
-                  child:FlatButton(
-                    onPressed:() {
-                      final player = AudioCache();
-                      player.play('note2.wav');
-                    },
-                  ),
-                ),
-                SizedBox(
-                  height: 10.0,
-                ),
-                Container(
-
-                  height: 75.0,
-
-                  color:Colors.deepOrangeAccent,
-
-
-                  child:FlatButton(
-                    onPressed:() {
-                      final player = AudioCache();
-                      player.play('note3.wav');
-                    },
-                  ),
-                ),
-                SizedBox(
-                  height: 10.0,
-                ),
-                Container(
-
-                  height: 75.0,
-                  width: double.infinity,
-                  color:Colors.greenAccent,
-
-
-                  child:FlatButton(
-                    onPressed:() {
-                      final player = AudioCache();
-                      player.play('note4.wav');
-                    },
-                  ),
-                ),
-                SizedBox(
-                  height: 10.0,
-                ),
-                Container(
-
-                  height: 75.0,
-
-                  color:Colors.indigo,
-
-
-                  child:FlatButton(
-                    onPressed:() {
-                      final player = AudioCache();
-                      player.play('note5.wav');
-                    },
-                  ),
-                ),
-                SizedBox(
-                  height: 10.0,
-                ),
-                Container(
-
-                  height: 75.0,
-
-                  color:Colors.blueGrey,
-
-
-                  child:FlatButton(
-                    onPressed:() {
-                      final player = AudioCache();
-                      player.play('note6.wav');
-                    },
-                  ),
-                ),
-                SizedBox(
-                  height: 10.0,
-                ),
-                Container(
-
-                  height: 75.0,
-
-                  color:Colors.teal,
-
-
-                  child:FlatButton(
-                    onPressed:() {
-                      final player = AudioCache();
-                      player.play('note7.wav');
-                    },
-                  ),
-                ),
-
-
-
-
-
-
+                 buildkey(color:Colors.red,soundnumber:1),
+                buildkey(color: Colors.yellowAccent,soundnumber: 2),
+                buildkey(color: Colors.blueAccent,soundnumber: 3),
+                buildkey(color: Colors.purpleAccent,soundnumber: 4),
+                buildkey(color: Colors.greenAccent,soundnumber: 5),
+                buildkey(color: Colors.lightBlueAccent,soundnumber: 6),
+                buildkey(color: Colors.orangeAccent,soundnumber: 7),
               ],
 
            ),
